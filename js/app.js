3ed07688931943a71e1dfa7d600fd5fd32993d6b
Gina Pultorak <gina.pultorak@gmail.com>
2019-12-03 12:07:42 -0800

    update gitignore with smaller gitignore content

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+console.log('The JS file is linked correctly.');
+
+var username = prompt('Tell me your name!');
+console.log('username = ' + username);
+
+// if the username is Gina, show a custom message, otherwise show a generic message
+
+var lowerCaseUserName = username.toLowerCase();
+
+if(lowerCaseUserName === 'gina') {
+  alert('Well, helloooo there, ' + username + 'banina!!!');
+} else {
+  alert('Welcome to my site!');
+}
+
